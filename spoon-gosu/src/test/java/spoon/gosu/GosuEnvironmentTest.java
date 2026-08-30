b2e748e04912307f0fe70929bca33745b838e69e
Patrick Kelley <patrick.david.kelley@gmail.com>
2026-08-30 05:01:43 -0400

    style: remove unused import in GosuEnvironmentTest

--- spoon-gosu/src/test/java/spoon/gosu/GosuEnvironmentTest.java
+++ spoon-gosu/src/test/java/spoon/gosu/GosuEnvironmentTest.java
@@ -10,7 +10,6 @@ package spoon.gosu;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
-import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
